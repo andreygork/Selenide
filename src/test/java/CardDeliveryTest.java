6d21176a840e04120edd38ec787a68cfc6639644
andreygork <agorkaev99@gmail.com>
2022-10-20 21:43:52 +0300

    add second test

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -14,7 +14,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class CardDeliveryTest {
 
     // String currentDate(int day) {
-      //  return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    //  return LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     //}
 
     @BeforeEach
@@ -28,7 +28,25 @@ public class CardDeliveryTest {
         String date = LocalDate.now().plusDays(3).format((DateTimeFormatter.ofPattern("dd.MM.yyyy")));
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id=date] input").sendKeys(date);
-        $("[data-test-id=name] input").setValue("Андрей");
+        $("[data-test-id=name] input").setValue("Андрей Горькаев");
+        $("[data-test-id=phone] input").setValue("+79017054075");
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Забронировать")).click();
+        $("[data-test-id=notification]").shouldBe(text("Успешно! Встреча успешно забронирована на "
+                + date), Duration.ofSeconds(15)).shouldBe(visible);
+    }
+
+    @Test
+    public void should() {
+        $("[data-test-id=city] input").setValue("Мо");
+        $$("[class=menu-item__control]").find(exactText("Москва")).click();
+        $("[data-test-id=date] input").click();
+        int days = 4;
+        for (int i = 0; i < days; i++) {
+            $(".calendar").sendKeys(Keys.ARROW_RIGHT);
+        }
+        String date = LocalDate.now().plusDays(7).format((DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        $("[data-test-id=name] input").setValue("Андрей Горькаев");
         $("[data-test-id=phone] input").setValue("+79017054075");
         $("[class=checkbox__box]").click();
         $$("[class=button__text]").find(exactText("Забронировать")).click();
