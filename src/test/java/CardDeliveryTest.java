ff4e60309999ddad8f190f80015c54bce3966de1
andreygork <agorkaev99@gmail.com>
2022-10-20 20:58:34 +0300

    Upd

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -34,20 +34,4 @@ public class CardDeliveryTest {
         $$("[class=button__text]").find(exactText("Забронировать")).click();
         $("[data-test-id=notification]").shouldBe(text("Успешно! Встреча успешно забронирована на "
                 + date), Duration.ofSeconds(15)).shouldBe(visible);
-    }
-    @Test
-    public void should(){
-        $("[data-test-id=city] input").setValue("Мо");
-        $$("[class=menu-item__control]") .find(exactText("Москва")).click();
-        String date = LocalDate.now().plusDays(3).format((DateTimeFormatter.ofPattern("dd.MM.yyyy")));
-        $("[data-test-id=date] input").click();
-        $("[")
-        $("[data-test-id=name] input").setValue("Андрей");
-        $("[data-test-id=phone] input").setValue("+79017054075");
-        $("[class=checkbox__box]").click();
-        $$("[class=button__text]").find(exactText("Забронировать")).click();
-        $("[data-test-id=notification]").shouldBe(text("Успешно! Встреча успешно забронирована на "
-                + date), Duration.ofSeconds(15)).shouldBe(visible);
-    }
-}
 
